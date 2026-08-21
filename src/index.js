6e21444ee3557504585e04b1dc01eea2380696c0
Kudratov1412 ishxona <furqatkudratov545@gmail.com>
2026-08-21 22:22:32 +0500

    3-qism Redux ni sozlab olish

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// Setting Redux
+import { createStore } from "redux";
+// Provider
+import { Provider } from "react-redux";
+// Importing Reducer
+import rootReducer from "./redux/reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
